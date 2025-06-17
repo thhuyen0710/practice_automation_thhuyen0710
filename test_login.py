c68be069a135f2fc155f6bb841fb4ed736e700ac
thhuyen0710 <th.huyen07102003@gmail.com>
2025-06-17 15:53:46 +0700

    Update test_login.py
    update

--- test_login.py
+++ test_login.py
@@ -11,7 +11,6 @@ class BaseTest:
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        print(f"\nURL: {self.driver.current_url}")
         request.cls.driver = self.driver
         yield
         self.driver.quit()
@@ -32,16 +31,9 @@ class TestWebLogin(BaseTest):
             EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
         )
         login_button.click()
-        time.sleep(5)
-        try:
-            dashboard_header_selector = ".oxd-topbar-header-title"
-            dashboard_header_element = wait.until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, dashboard_header_selector))
+        dashboard_header_selector = ".oxd-topbar-header-title"
+        dashboard_header_element = wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, dashboard_header_selector))
             )
-            assert dashboard_header_element.text == "Dashboard", \
-                f"Expected dashboard header text 'Dashboard', but got '{dashboard_header_element.text}'"
-            print("Login successful!")
-
-        except Exception as e:
-            print(f"Login fail!: {e}")
-            pytest.fail(f"Cannot verify Dashboard screen. Error: {e}")
+        assert dashboard_header_element.text == "Dashboard"
+        print("Login successful!")
