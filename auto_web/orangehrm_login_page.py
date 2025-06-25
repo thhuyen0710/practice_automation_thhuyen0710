04fa83dfbccd5ec8949ef8e9a241277e819205b7
thhuyen0710 <th.huyen07102003@gmail.com>
2025-06-25 13:36:56 +0700

    Update orangehrm_login_page.py

--- auto_web/orangehrm_login_page.py
+++ auto_web/orangehrm_login_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait    
 from selenium.webdriver.support import expected_conditions as EC   
 
-class LoginScreen: #Lớp đại diện cho trang đăng nhập
+class LoginScreen:
     def __init__(self, driver):     
         self.driver = driver   
         self.wait = WebDriverWait(driver, 15)  
